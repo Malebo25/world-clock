59dddb159df3a561475d6cca64642c53db419199
malebo25 <malebomotlhake@gmail.com>
2025-09-04 09:56:24 +0200

    back link to default page code + footer decoration

--- index.js
+++ index.js
@@ -46,6 +46,9 @@ function displayCity(event) {
   let time = moment().tz(timeZone).format("HH:mm [<small>]A[</small>]");
 
   let displayElement = document.querySelector("#cities");
+  // Default city view
+  let originalCitiesHTML = document.querySelector("#cities").innerHTML;
+
   displayElement.innerHTML = `
          <div class="city" >
           <div>
@@ -54,10 +57,19 @@ function displayCity(event) {
           </div>
 
           <div class="time">${time}</div>
-        </div>
+          
+          </div>
+          <div class="back"><a href="#" id="back-link"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+</svg>back</a></div>
   `;
+
+  let backLink = document.querySelector("#back-link");
+  backLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    displayElement.innerHTML = originalCitiesHTML;
+  });
 }
-setInterval(displayCity, 1000);
 
 let citySelection = document.querySelector("#city");
 citySelection.addEventListener("change", displayCity);
